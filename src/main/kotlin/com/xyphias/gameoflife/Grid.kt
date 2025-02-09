9c5bb5febb671165701cbb23c76e3824532b34c1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-02-09 12:12:42 +0000

    Lay code out vertically

--- src/main/kotlin/com/xyphias/gameoflife/Grid.kt
+++ src/main/kotlin/com/xyphias/gameoflife/Grid.kt
@@ -3,13 +3,17 @@ package com.xyphias.gameoflife
 class Grid(val liveCells: Set<Cell>) {
     fun next(): Grid {
         val survivors = 
-            liveCells.withNeighbourCount(2).plus(liveCells.withNeighbourCount(3))
+            liveCells
+                .withNeighbourCount(2)
+                .plus(liveCells.withNeighbourCount(3))
         
         return Grid(survivors)
     }
 
     private fun Set<Cell>.withNeighbourCount(count: Int): Set<Cell> =
-        this.filter { cell -> neighboursOf(cell).size == count }.toSet()
+        this
+            .filter { cell -> neighboursOf(cell).size == count }
+            .toSet()
 
     private fun neighboursOf(cell: Cell): Set<Cell> = 
         cell
