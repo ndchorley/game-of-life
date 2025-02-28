8a208aa1df801c8807ad4a0181845e9c692265be
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-02-28 13:04:36 +0000

    Wire in RequestPrintingFilter

--- src/main/kotlin/com/xyphias/gameoflife/main.kt
+++ src/main/kotlin/com/xyphias/gameoflife/main.kt
@@ -1,11 +1,18 @@
 package com.xyphias.gameoflife
 
 import com.xyphias.gameoflife.web.GameOfLifeApp
+import com.xyphias.gameoflife.web.RequestPrintingFilter
+import org.http4k.core.then
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.time.Clock
 
 fun main() {
-    GameOfLifeApp()
+    val app = 
+        RequestPrintingFilter(Clock.systemUTC(), ::println)
+            .then(GameOfLifeApp())
+    
+    app
         .asServer(Jetty(port = 8080))
         .start()
 }
