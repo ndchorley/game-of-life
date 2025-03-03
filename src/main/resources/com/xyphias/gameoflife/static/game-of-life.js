70a2677baea85f0b3ec5f23e2ca7b34d19c052bf
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-03-03 16:13:02 +0000

    Add an I pattern

--- src/main/resources/com/xyphias/gameoflife/static/game-of-life.js
+++ src/main/resources/com/xyphias/gameoflife/static/game-of-life.js
@@ -13,7 +13,16 @@ const glider = {
         {x: 4, y: 2}, {x: 4, y: 3}, {x: 4, y: 4},
         {x: 3, y: 4}, {x: 2, y: 3}
     ]
-}
+};
+
+const i = {
+    sideLength: gridSize,
+    liveCells: [
+        {x: 4, y: 3}, {x: 5, y: 3}, {x: 6, y: 3},
+        {x: 5, y: 4}, {x: 5, y: 5}, {x: 5, y: 6},
+        {x: 4, y: 6}, {x: 6, y: 6}
+    ]
+};
 
 function drawVerticalLine(context, x) {
     context.moveTo(x, 0)
@@ -114,5 +123,7 @@ document.getElementById("pattern")
             setUpPattern(blinker);
         } else if (pattern === "glider") {
             setUpPattern(glider);
+        } else if (pattern === "i") {
+            setUpPattern(i);
         }
     });
