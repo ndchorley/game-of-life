657fcec14265c3d9afcb6549d3d51656d0204e52
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-03-08 19:25:44 +0000

    Run the GameOfLifeContract against the running app, too

---
+++ src/test/kotlin/com/xyphias/gameoflife/web/RunningAppTests.kt
@@ -0,0 +1,28 @@
+package com.xyphias.gameoflife.web
+
+import com.xyphias.gameoflife.GameOfLife
+import com.xyphias.gameoflife.GameOfLifeContract
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.Uri
+import org.http4k.core.then
+import org.http4k.filter.ClientFilters
+import org.junit.jupiter.api.BeforeAll
+
+class RunningAppTests : GameOfLifeContract {
+    companion object {
+        val port = 8080
+
+        @BeforeAll
+        @JvmStatic
+        fun startApp() {
+            Server(port).start()
+        }
+    }
+    
+    private val client =
+        ClientFilters.SetBaseUriFrom(Uri.of("http://localhost:$port"))
+            .then(JavaHttpClient())
+    
+    override val gameOfLife: GameOfLife = HttpGameOfLife(client)
+}
+
