8cf469e10291ab61cf82f357f02ace4e6f16e79b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-03-30 06:45:19 +0100

    Upgrade http4k to 6.4.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.http4k:http4k-bom:6.1.0.1"))
+    implementation(platform("org.http4k:http4k-bom:6.4.1.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-format-moshi")
     implementation("org.http4k:http4k-server-jetty")
