24b70d278bedd9199620ef3a2e0f9c4e1c169d3c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-03-30 06:49:13 +0100

    Upgrade Kotlin to 2.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
     id("application")
     id("com.gradleup.shadow") version "8.3.6"
     id("com.adarshr.test-logger") version "4.0.0"
